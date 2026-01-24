5f35e2c56f466a874a1b90bdd60411694000725b
PaulMatuto <cpe.matuto.paulaaron@gmail.com>
2026-01-24 09:13:58 +0800

    Add project files.

---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Globalization.DateTimeFormatting;
+
+namespace LibraryManagementSystem.Models
+{
+    class Book
+    {
+        public int BookId { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Genre { get; set; }
+        public YearFormat YearPublished { get; set; }
+        public int Quantity { get; set; }
+    }
+}
